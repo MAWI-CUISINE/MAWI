5ec398cf96f7997306fe317d1579e30fd1932b8a
WiemMimouni <wiemmimouni74@gmail.com>
2022-12-05 14:35:10 +0100

    hello

--- mawi/src/App.jsx
+++ mawi/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route } from "react-router-dom";
 import Login from './components/login/Login.jsx';
 import SignUp from './components/login/SignUp.jsx';
 import Home from './components/user/Home.jsx';
+import ARecipeDetails from "./components/Admin/ARecipeDetails"
 
  function App() {
   return (
@@ -10,6 +11,7 @@ import Home from './components/user/Home.jsx';
     <div>
       <Routes>
         <Route>
+        <Route path="/admin/RecipeDetails" element={<ARecipeDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
