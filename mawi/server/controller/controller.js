66d0bbafd1d7d62a9c43ba7d7c94876f693449fc
amineamdouni <amineamdouni24@gmail.com>
2022-12-06 15:09:48 +0100

    login done

--- mawi/server/controller/controller.js
+++ mawi/server/controller/controller.js
@@ -1,22 +1,46 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const {Post}=require("../database")
+const { User, Admin, Recipe, Post, Shop } = require("../database");
 
-const { model } = require("mongoose");
-// const SignUp= async(req,res)=>{
-//     const body=req.body
-//     try{
-// const Password= await bcrypt.hash(body.Password,10)
-// await User.create({
-//     Uname:body.username,
-//     Uemail:body.email,
-//     Upassword:body.Password})
-// }catch(err){}
-// }
 
+const SignUp= async(req,res)=>{
+    let body=req.body
+    try{
+const Password= await bcrypt.hash(body.password,10)
+await User.create({
+    Uname:body.username,
+    Uemail:body.email,
+    Upassword:Password},(err,result)=>{
+      if (err)console.log(err);
+      else res.json(result);
+    })
+}catch(err){
+  console.log(err);
+}
+}
+
+const Login=async(req,res)=>{
+  let body=req.body
+
+const user=await User.findOne({Uname:body.username})
+
+if (!user){
+  return { status:'error',error:'username not found'}
+}
+let Check=await bcrypt.compare(
+  body.password,user.Upassword
+)
+if(Check){
+  const token=jwt.sign({name:user.Uname,email:user.Uemail},'topsecret')
+  return res.json({user:token,status:'all good'})
+}
+else{
+  return res.json({status:'error',user:false})
+}
+}
 
 const post=async(req,res)=>{
-    const body=req.body
+    let body=req.body
     try{
       await  Post.create(body,(err,result)=>{
         if (err) res.json(err)
@@ -27,4 +51,4 @@ const post=async(req,res)=>{
         console.log(err);
     }
 }
-module.exports = {post}
+module.exports = {post,SignUp,Login}
--- mawi/server/routes/route.js
+++ mawi/server/routes/route.js
@@ -1,5 +1,11 @@
+const Mawirouter = require("express").Router();
+const { post,SignUp,Login } = require("../controller/controller");
 
-const Mawirouter = require('express').Router();
-const {post}=require('../controller/controller')
-Mawirouter.route('/post').post(post)
-module.exports =Mawirouter
+
+
+Mawirouter.route("/post").post(post);
+Mawirouter.route("/signup").post(SignUp);
+Mawirouter.route("/login").post(Login);
+
+
+module.exports = Mawirouter;
