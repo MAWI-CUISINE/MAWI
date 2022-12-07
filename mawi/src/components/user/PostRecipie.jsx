4be8ad42cb3fc8de7b5938eddbce92bfb6709187
amineamdouni <amineamdouni24@gmail.com>
2022-12-07 11:28:31 +0100

    check done

--- mawi/src/components/user/PostRecipie.jsx
+++ mawi/src/components/user/PostRecipie.jsx
@@ -21,7 +21,7 @@ const PostRecipie = () => {
     return (
 
         <div>
-            {/* { <!-- ##### Breadcumb Area Start ##### --> } */}
+            {/* { <!-- ///// Breadcumb Area Start ///// --> } */}
             <div
                 className="breadcumb-area bg-img bg-overlay"
                 style={{
@@ -45,9 +45,9 @@ const PostRecipie = () => {
                     <div className="container">
 
 
-//                         <div className="row">
-//                             <div className="col-12 col-lg-8">
-//                             </div>
+                         <div className="row">
+                            <div className="col-12 col-lg-8">
+                            </div>
 
 
                             {/* <!-- Ingredients --> */}
@@ -166,169 +166,167 @@ const PostRecipie = () => {
             </div>
 
 
-//             {/* <!-- ##### Follow Us Instagram Area Start ##### --> */}
-//             <div className="follow-us-instagram">
-//                 <div className="container">
-//                     <div className="row">
-//                         <div className="col-12">
-//                             <h5>Follow Us Instragram</h5>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 {/* <!-- Instagram Feeds --> */}
-//                 <div className="insta-feeds d-flex flex-wrap">
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670248932/mawi%20cuisine/insta1_qlet3h.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
+            {/* <!-- ///// Follow Us Instagram Area Start ///// --> */}
+            <div className="follow-us-instagram">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h5>Follow Us Instragram</h5>
+                        </div>
+                    </div>
+                </div>
+                {/* <!-- Instagram Feeds --> */}
+                <div className="insta-feeds d-flex flex-wrap">
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670248932/mawi%20cuisine/insta1_qlet3h.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
 
-//                             </a>
-//                         </div>
-//                     </div>
+                            </a>
+                        </div>
+                    </div>
 
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670248989/mawi%20cuisine/insta2_uwrmlo.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
-//                                 <i className="fa fa-instagram" aria-hidden="true"></i>
-//                             </a>
-//                         </div>
-//                     </div>
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670248989/mawi%20cuisine/insta2_uwrmlo.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
 
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249063/mawi%20cuisine/insta3_ehnaae.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
-//                                 <i className="fa fa-instagram" aria-hidden="true"></i>
-//                             </a>
-//                         </div>
-//                     </div>
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249063/mawi%20cuisine/insta3_ehnaae.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
 
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249083/mawi%20cuisine/insta4_nd6wrw.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
-//                                 <i className="fa fa-instagram" aria-hidden="true"></i>
-//                             </a>
-//                         </div>
-//                     </div>
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249083/mawi%20cuisine/insta4_nd6wrw.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
 
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249105/mawi%20cuisine/insta5_oyivr8.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
-//                                 <i className="fa fa-instagram" aria-hidden="true"></i>
-//                             </a>
-//                         </div>
-//                     </div>
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249105/mawi%20cuisine/insta5_oyivr8.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
 
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249126/mawi%20cuisine/insta6_rlzig6.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
-//                                 <i className="fa fa-instagram" aria-hidden="true"></i>
-//                             </a>
-//                         </div>
-//                     </div>
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249126/mawi%20cuisine/insta6_rlzig6.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
 
-//                     {/* <!-- Single Insta Feeds --> */}
-//                     <div className="single-insta-feeds">
-//                         <img
-//                             src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249146/mawi%20cuisine/insta7_rjzno4.jpg"
-//                             alt=""
-//                         />
-//                         {/* <!-- Icon --> */}
-//                         <div className="insta-icon">
-//                             <a href="#">
-//                                 <i className="fa fa-instagram" aria-hidden="true"></i>
-//                             </a>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//             {/* <!-- ##### Follow Us Instagram Area End ##### --> */}
+                    {/* <!-- Single Insta Feeds --> */}
+                    <div className="single-insta-feeds">
+                        <img
+                            src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249146/mawi%20cuisine/insta7_rjzno4.jpg"
+                            alt=""
+                        />
+                        {/* <!-- Icon --> */}
+                        <div className="insta-icon">
+                            <a href="/">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- ///// Follow Us Instagram Area End ///// --> */}
 
-//             {/* <!-- ##### Footer Area Start ##### --> */}
-//             <footer className="footer-area">
-//                 <div className="container h-100">
-//                     <div className="row h-100">
-//                         <div className="col-12 h-100 d-flex flex-wrap align-items-center justify-content-between">
-//                             {/* <!-- Footer Social Info --> */}
-//                             <div className="footer-social-info text-right">
-//                                 <a href="#">
-//                                     <i className="fa fa-pinterest" aria-hidden="true"></i>
-//                                 </a>
-//                                 <a href="#">
-//                                     <i className="fa fa-facebook" aria-hidden="true"></i>
-//                                 </a>
-//                                 <a href="#">
-//                                     <i className="fa fa-twitter" aria-hidden="true"></i>
-//                                 </a>
-//                                 <a href="#">
-//                                     <i className="fa fa-dribbble" aria-hidden="true"></i>
-//                                 </a>
-//                                 <a href="#">
-//                                     <i className="fa fa-behance" aria-hidden="true"></i>
-//                                 </a>
-//                                 <a href="#">
-//                                     <i className="fa fa-linkedin" aria-hidden="true"></i>
-//                                 </a>
-//                             </div>
-//                             {/* <!-- Footer Logo --> */}
-//                             <div className="footer-logo">
-//                                 <a href="index.html">
-//                                     <img
-//                                         src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249613/mawi%20cuisine/logo_cptyxq.png"
-//                                         alt=""
-//                                     />
-//                                 </a>
-//                             </div>
-//                             {/* <!-- Copywrite --> */}
-//                             {/* <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
-//                             Copyright &copy;
-//                             <script>document.write(new Date().getFullYear());</script> All
-//                             rights reserved | This template is made with{" "}
-//                             <i className="fa fa-heart-o" aria-hidden="true"></i> by{" "}
-//                             <a href="https://colorlib.com" target="_blank">
-//                                 Colorlib
-//                             </a>
-//                             {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p> */}
-//                         </div>
-//                     </div>
-//                 </div>
-//             </footer>
-//         </div>
-//     )
-// }
+            {/* <!-- ///// Footer Area Start ///// --> */}
+            <footer className="footer-area">
+                <div className="container h-100">
+                    <div className="row h-100">
+                        <div className="col-12 h-100 d-flex flex-wrap align-items-center justify-content-between">
+                            {/* <!-- Footer Social Info --> */}
+                            <div className="footer-social-info text-right">
+                                <a href="/">
+                                    <i className="fa fa-pinterest" aria-hidden="true"></i>
+                                </a>
+                                <a href="/">
+                                    <i className="fa fa-facebook" aria-hidden="true"></i>
+                                </a>
+                                <a href="/">
+                                    <i className="fa fa-twitter" aria-hidden="true"></i>
+                                </a>
+                                <a href="/">
+                                    <i className="fa fa-dribbble" aria-hidden="true"></i>
+                                </a>
+                                <a href="/">
+                                    <i className="fa fa-behance" aria-hidden="true"></i>
+                                </a>
+                                <a href="/">
+                                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                                </a>
+                            </div>
+                            {/* <!-- Footer Logo --> */}
+                            <div className="footer-logo">
+                                <a href="index.html">
+                                    <img
+                                        src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670249613/mawi%20cuisine/logo_cptyxq.png"
+                                        alt=""
+                                    />
+                                </a>
+                            </div>
+                            {/* <!-- Copywrite --> */}
+                            {/* <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
+                            Copyright &copy;
+                            <script>document.write(new Date().getFullYear());</script> All
+                            rights reserved | This template is made with{" "}
+                            <i className="fa fa-heart-o" aria-hidden="true"></i> by{" "}
+                          
+                            {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p> */}
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
 
-// export default PostRecipie
+export default PostRecipie
